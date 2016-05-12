48a1f3d6e5eda4e143ba3c33eab06546db3eead7
maulikpokiya <maulik.pokiya@gmail.com>
2016-05-11 21:05:44 -0400

    Class Node

---
+++ DataStructures/Node.java
@@ -0,0 +1,33 @@
+package me.maulikpokiya.datastructures;
+
+public class Node {
+
+	int data;
+	Node next;
+	
+	public Node(int val) {
+		this.data = val;
+		this.next = null;
+	}
+	
+	public Node(int val, Node next) {
+		this.data = val;
+		this.next = next;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+}
