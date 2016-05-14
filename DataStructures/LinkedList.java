381ecdb6dfb2e0372ddf5d972bb5e03b9f58878a
maulikpokiya <maulik.pokiya@gmail.com>
2016-05-14 14:08:36 -0400

    Adding iterator to LinkedList

--- DataStructures/LinkedList.java
+++ DataStructures/LinkedList.java
@@ -1,4 +1,6 @@
-public class LinkedList<T> {
+import java.util.Iterator;
+
+public class LinkedList<T> implements Iterable<T> {
 
 	Node<T> head;
 
@@ -9,115 +11,145 @@ public class LinkedList<T> {
 	public LinkedList(Node<T> head) {
 		this.head = head;
 	}
-	
+
 	public Node<T> searchNode(T val) {
-		
+
 		Node<T> temp = head;
-		while(temp!=null && temp.data!=val) {
+		while (temp != null && temp.data != val) {
 			temp = temp.next;
 		}
 		return temp;
 	}
-	
+
 	public Node<T> addFirst(T val) {
 
 		head = new Node<T>(val, head);
 		return head;
 	}
-	
+
 	public void traverse() {
 		Node<T> temp = head;
-		while(temp!=null)
+		while (temp != null)
 			temp = temp.next;
 	}
-	
+
 	public Node<T> addLast(T val) {
 		Node<T> temp = head;
-		
-		if(temp==null)
+
+		if (temp == null)
 			addFirst(val);
 		else {
-			while(temp.next!=null) {
+			while (temp.next != null) {
 				temp = temp.next;
 			}
 			temp.next = new Node<T>(val);
 		}
 		return temp;
 	}
-	
+
 	public Node<T> insertAfter(T key, T val) {
 		Node<T> temp = head;
-		if(temp == null)
+		if (temp == null)
 			System.out.println("Key not found");
 		else {
-			while(temp!=null && temp.data!=key)
+			while (temp != null && temp.data != key)
 				temp = temp.next;
-			if(temp == null)
+			if (temp == null)
 				System.out.println("Key not found");
 			else
 				temp.next = new Node<T>(val, temp.next);
 		}
 		return temp.next;
 	}
-	
+
 	public Node<T> insertBefore(T key, T val) {
 		Node<T> temp = head;
 		Node<T> prev = null;
-		
-		if(temp==null) {
+
+		if (temp == null) {
 			throw new IndexOutOfBoundsException();
 		} else {
-			while(temp!=null && temp.data != key) {
+			while (temp != null && temp.data != key) {
 				prev = temp;
 				temp = temp.next;
 			}
-			if(temp==null)
+			if (temp == null)
 				throw new IndexOutOfBoundsException();
-			else if(prev==null)
-				addFirst(val);
+			else if (prev == null)
+				prev = addFirst(val);
 			else
 				prev.next = new Node<T>(val, temp);
 		}
 		return prev.next;
 	}
-	/*
+
 	public Node<T> remove(T val) {
-		
-	}
-	
-	public Node deleteNode(int val) {
-		Node temp = head;
-		Node del = null;
-		
-		if(temp!=null && temp.data == val) {
-			
-			del = temp;
+
+		Node<T> temp = head;
+		Node<T> del = null;
+
+		if (temp != null && temp.data == val) {
 			head = temp.next;
-			del.next=null;
-		} else {
-			while(temp.next!=null && temp.next.data!=val) {
-				temp = temp.next;
-			}
-			
-			if(temp.next.data == val) {
-				del = temp.next;
-				temp.next=del.next;
-				del.next=null;
-			}
+			temp.next = null;
+			return temp;
 		}
-		
+
+		while (temp.next != null && temp.next.data != val) {
+			temp = temp.next;
+		}
+
+		if (temp.next.data == val) {
+			del = temp.next;
+			temp.next = del.next;
+			del.next = null;
+		}
+
 		return del;
 	}
-	*/
+
 	public void printList() {
-		
+
 		Node<T> temp = head;
-		
-		while(temp!=null) {
+
+		while (temp != null) {
 			System.out.print(temp.data + " ");
 			temp = temp.next;
 		}
 		System.out.print("\n");
 	}
-}
 
+	public Iterator<T> iterator() {
+		return new LinkedListIterator();
+	}
+
+	private class LinkedListIterator implements Iterator<T> {
+
+		private Node<T> nextNode;
+
+		public LinkedListIterator() {
+			nextNode = head;
+		}
+
+		@Override
+		public boolean hasNext() {
+			if (nextNode != null)
+				return true;
+			return false;
+		}
+
+		@Override
+		public T next() {
+			if (hasNext()) {
+				T res = nextNode.data;
+				nextNode = nextNode.next;
+				return res;
+			}
+			return null;
+		}
+
+		@Override
+		public void remove() {
+
+		}
+	}
+}
